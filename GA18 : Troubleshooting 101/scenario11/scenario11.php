3ea8f3470c9a2cb4d9c1ffd87a763a5a3b8caf33
Drxiee <johndrexfirma.cantor24@bicol-u.edu.ph>
2025-11-21 22:01:18 +0800

    Add files via upload

--- GA18 : Troubleshooting 101/scenario11/scenario11.php
+++ GA18 : Troubleshooting 101/scenario11/scenario11.php
@@ -1,3 +1,10 @@
 <?php
-$email = $_GET['email'];
+// Form uses GET method, so we use $_GET instead of $_POST
+$email = $_GET['email'] ?? '';
+
+if (!empty($email)) {htmlspecialchars($email);
+    echo "Email received via GET"; 
+} else {
+    echo "Please enter an email address.";
+}
 ?>
